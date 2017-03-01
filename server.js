9ed3fa3f7b292d17c50facd676980582408222fa
Kim Hart <kimhart90@gmail.com>
2017-02-28 20:38:41 -0500

    added procfile

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/', express.static('public'));
 
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
